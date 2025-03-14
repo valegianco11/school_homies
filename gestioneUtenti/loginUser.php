a7bf1a1539d24142c07a97bcdb982ce137bec097
valegianco11 <127424052+valegianco11@users.noreply.github.com>
2025-03-14 15:18:33 +0100

    Update loginUser.php
    mwah

--- gestioneUtenti/loginUser.php
+++ gestioneUtenti/loginUser.php
@@ -12,14 +12,23 @@ $result = $stmt->get_result();
 //verifica se l'utente esiste e se le credenziali sono corrette
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    session_start();
-    $_SESSION['id_user'] = $row['id_user'];
-    $_SESSION['email'] = $email;
-    $_SESSION['name'] = $row['name'];
-    $_SESSION['surname'] = $row['surname'];
-    // Passa i dati dell'utente alla pagina index.php
-    header("Location: ../index.php");
-    exit();
+    $hashed_password = $row['password'];
+
+    // Verifica la password
+    if (password_verify($password, $hashed_password)) {
+        session_start();
+        $_SESSION['id_user'] = $row['id_user'];
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['surname'] = $row['surname'];
+
+        // Passa i dati dell'utente alla pagina index.php
+        header("Location: ../index.php");
+        exit();
+    } else {
+        // Password non valida
+        echo "Email o password non validi. <a href='../registrazione.php'>Non hai un account? Registrati</a>";
+    }
 } else {
     // Email non valida
     echo "Email o password non validi. <a href='../registrazione.php'>Non hai un account? Registrati</a>";
